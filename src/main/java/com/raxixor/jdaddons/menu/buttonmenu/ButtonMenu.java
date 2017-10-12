78116ed275b8041c2c1316846253afeffa749dc2
raxixor <raxixor@gmail.com>
2017-10-12 18:52:54 +0300

    ButtonMenu complete, now to work on ButtonMenuBuilder.

--- src/main/java/com/raxixor/jdaddons/menu/buttonmenu/ButtonMenu.java
+++ src/main/java/com/raxixor/jdaddons/menu/buttonmenu/ButtonMenu.java
@@ -2,9 +2,12 @@ package com.raxixor.jdaddons.menu.buttonmenu;
 
 import com.raxixor.jdaddons.command.EventWaiter;
 import com.raxixor.jdaddons.menu.Menu;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.entities.MessageReaction.ReactionEmote;
-import net.dv8tion.jda.core.entities.Role;
-import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.core.requests.RestAction;
 
 import java.awt.Color;
 import java.util.List;
@@ -31,5 +34,58 @@ public class ButtonMenu extends Menu {
         this.choices = choices;
         this.action = action;
         this.cancel = cancel;
+    }
+    
+    @Override
+    public void display(MessageChannel channel) {
+        initialize(channel.sendMessage(getMessage()));
+    }
+    
+    @Override
+    public void display(Message message) {
+        initialize(message.editMessage(getMessage()));
+    }
+    
+    private void initialize(RestAction<Message> ra) {
+        ra.queue(m -> {
+            for (int i = 0; i < choices.size(); i++) {
+                Emote emote;
+                try {
+                    emote = m.getJDA().getEmoteById(choices.get(i));
+                } catch (Exception e) {
+                    emote = null;
+                }
+                RestAction<Void> r = emote == null ?
+                        m.addReaction(choices.get(i)) :
+                        m.addReaction(emote);
+                if (i + 1 < choices.size())
+                    r.queue();
+                else
+                    r.queue(v -> {
+                        waiter.waitForEvent(MessageReactionAddEvent.class, event -> {
+                            if (!event.getMessageId().equals(m.getId()))
+                                return false;
+                            String re = event.getReaction().getEmote().isEmote()
+                                    ? event.getReaction().getEmote().getId()
+                                    : event.getReaction().getEmote().getName();
+                            if (!choices.contains(re))
+                                return false;
+                            return isValidUser(event);
+                        }, (MessageReactionAddEvent event ) -> {
+                            m.delete().queue();
+                            action.accept(event.getReaction().getEmote());
+                        }, timeout, unit, cancel);
+                    });
+            }
+        });
+    }
+    
+    private Message getMessage() {
+        MessageBuilder m = new MessageBuilder();
+        if (text != null)
+            m.append(text);
+        if (description != null)
+            m.setEmbed(new EmbedBuilder().setColor(color).setDescription(description).build());
+        return m.build();
     }
 }
